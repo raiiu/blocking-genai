ef9158a8d5a330901c68fc095dac654be094d47e
Steed <53194917+Squarerootofpi@users.noreply.github.com>
2024-09-10 09:09:03 -0400

    Create blockall.ps1

---
+++ blockall.ps1
@@ -0,0 +1,25 @@
+# Define the paths
+$hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
+$backupHostsPath = "$env:SystemRoot\System32\drivers\etc\oldhosts"
+$addContentHostsPath = "$env:SystemRoot\System32\drivers\etc\addcontenthosts"
+
+# Create the addcontenthosts file with the desired content
+$content = @"
+127.0.0.1 chat.openai.com
+127.0.0.1 copilot.microsoft.com
+127.0.0.1 gemini.google.com
+127.0.0.1 character.ai
+127.0.0.1 chatpdf.com
+127.0.0.1 writesonic.com
+127.0.0.1 pi.ai
+"@
+Set-Content -Path $addContentHostsPath -Value $content -Force
+
+# Backup the current hosts file
+Copy-Item -Path $hostsPath -Destination $backupHostsPath -Force
+
+# Append the content from addcontenthosts to the hosts file
+Add-Content -Path $hostsPath -Value (Get-Content $addContentHostsPath)
+
+# Output a success message
+Write-Host "Content added to hosts file and old hosts file backed up as 'oldhosts'."
